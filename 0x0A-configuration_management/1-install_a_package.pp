842caa95e65a085849da3bc113a973609b36c1af
russiantech <chrisjsmez@gmail.com>
2023-11-24 22:01:07 -0800

    1-install_a_package.pp fixed reequirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,8 @@
-# 1-install_a_package.pp
-
-# Ensure pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
+#!/usr/bin/pup
 # Install Flask version 2.1.0 using pip3
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
 
