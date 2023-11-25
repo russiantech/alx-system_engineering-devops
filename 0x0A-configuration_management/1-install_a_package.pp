470594e0f9e59fe5e8a3b5ac473a92707c3a73ab
russiantech <chrisjsmez@gmail.com>
2023-11-24 22:12:52 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,19 @@
 #!/usr/bin/pup
-# Install Flask version 2.1.0 using pip3
 
-package { 'flask':
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install Flask 2.1.0 using pip3
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install Werkzeug 2.1.1 using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
 
