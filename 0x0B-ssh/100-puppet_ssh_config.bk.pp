18e6cdc3809035f79a18e681accf0242f321a9b1
russiantech <chrisjsmez@gmail.com>
2023-12-03 22:27:08 -0800

    puppeting

---
+++ 0x0B-ssh/100-puppet_ssh_config.bk.pp
@@ -0,0 +1,12 @@
+# Puppet manifest configure SSH client
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,16 @@
-# Puppet manifest to configure SSH client
+# Seting puppet client config file
 include stdlib
 
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
+file_line { 'Delare identity file':
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
