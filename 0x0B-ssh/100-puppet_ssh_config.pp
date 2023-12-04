056613fcfc4992855c58356169d95a08ad72b2ea
russiantech <chrisjsmez@gmail.com>
2023-12-03 20:39:39 -0800

    0x0B. SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Puppet manifest to configure SSH client
+
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
