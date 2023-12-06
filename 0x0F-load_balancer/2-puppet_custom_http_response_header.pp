b24a88acfa801de093bcdbd3885e1cea40ff2605
russiantech <chrisjsmez@gmail.com>
2023-12-06 03:15:22 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,7 @@
-#!/usr/bin/env bash
-# automate task 0 using nginx
-
-# Install Nginx
-class { 'nginx':
-  ensure => 'installed',
+exec { 'configure_custom_header':
+  command  => "apt-get -y update && apt-get -y install nginx && sed -i '/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default && service nginx restart",
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin:/sbin',
+  unless   => 'grep -q "add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+  require  => Package['nginx'],
 }
-
-# Create a custom HTTP header file
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure  => present,
-  content => "# Custom HTTP header configuration\nadd_header X-Served-By $hostname;\n",
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => File['/etc/nginx/conf.d/custom_headers.conf'],
-}
-
